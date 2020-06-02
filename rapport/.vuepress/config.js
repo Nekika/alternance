50b32fa277477c40deff7a2dfa7c5ee377c9fb2a
Nekika <hugo.jahnke@gmail.com>
2020-06-02 14:31:48 +0200

    Ajout de la sidebar

--- rapport/.vuepress/config.js
+++ rapport/.vuepress/config.js
@@ -1,4 +1,7 @@
 module.exports = {
     title: 'Suivi d\'alternance',
-    description: 'Ce rapport présentera l\'institution et le service au sein desquels l\'alternance a été réalisée.'
+    description: 'Ce rapport présentera l\'institution et le service au sein desquels l\'alternance a été réalisée.',
+    themeConfig: {
+        sidebar: 'auto'
+    }
 }

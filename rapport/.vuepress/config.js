21789915eb885cb6358fa7a6c35856b65e54599e
Nekika <hugo.jahnke@gmail.com>
2020-06-02 16:34:24 +0200

    Ajout de la navbar

--- rapport/.vuepress/config.js
+++ rapport/.vuepress/config.js
@@ -2,6 +2,11 @@ module.exports = {
     title: 'Suivi d\'alternance',
     description: 'Ce rapport présentera l\'institution et le service au sein desquels l\'alternance a été réalisée.',
     themeConfig: {
-        sidebar: 'auto'
+        sidebar: 'auto',
+        nav: [
+            { text: 'A propos', link: '/' },
+            { text: 'Bourgs-Centres', link: '/bourgs-centres/' }
+        ],
+        smoothScroll: true
     }
 }
